6d1751b76e26655ec2fdd1a12b28f110c3944904
Aleksey <alesha@gilenko.net>
2022-01-12 21:42:00 +0300

    update RecieverMainScript.py

--- RecieverMainScript.py
+++ RecieverMainScript.py
@@ -3,5 +3,5 @@ from CrRadio.CrRadio import CrRadio
 from CrRadio.RadioEnvironment import *
 
 crRadio = CrRadio(placement=0, debug=True)
-crRadio.recieveFile()
+crRadio.recieveFile("./recievedFile.b64")
 # crRadio.sendFile(os.path.abspath("./test.b64"))
