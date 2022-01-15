cd40c0517dc2a95dbf154f1bc17335a59a854e04
Aleksey <alesha@gilenko.net>
2022-01-15 13:00:29 +0300

    update RecieveLinealGround.py and SendLinealCamera.py

--- SendLinealCamera.py
+++ SendLinealCamera.py
@@ -15,7 +15,7 @@ pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
 radio = NRF24(GPIO, spidev.SpiDev())
 radio.begin(0, 5)
 time.sleep(1)
-radio.setRetries(3, 3)
+radio.setRetries(5, 5)
 radio.setPayloadSize(32)
 radio.setChannel(0x60)
 
